9bebfe2bb82921069a26c1ac441eb4b17fbd6c42
leerssej <jens.leerssen@gmail.com>
2017-06-25 22:04:24 -0700

    snake case conversion into function & slogging strt

--- camelCase_snake_case_convert.R
+++ camelCase_snake_case_convert.R
@@ -21,11 +21,36 @@ library(magrittr)
 createUpdate_CEDS6_TSQL <- read_lines("TSQL_camelCase/TSQL_camelCase_completeDebuggedCreateInsert_proofed.sql")
 
 ###### 2. Transform camels to snakes ######
-#  humps can be left until last or when the pg_sql ignores them on the transform out of db_schema
+##  humps can be left until last or when the pg_sql ignores them on the transform out of db_schema
+
+# The main transform
 cu_ceds6_tsql_sc <- gsub("([a-z])([A-Z])", "\\1_\\2", createUpdate_CEDS6_TSQL, perl = T)
+# New Identity Resolution
 cu_ceds6_tsql_sc %<>% gsub("\\[CEDS-TSQL-V6\\]",  "\\[CEDS_TSQL_v6sc\\]", .)
-cu_ceds6_tsql_sc %<>% gsub("_EL([A-Z])",  "_EL_\\1", ., perl = T)
-cu_ceds6_tsql_sc %<>% gsub("_AE([A-Z])",  "_AE_\\1", ., perl = T)
+
+# Sectional Issues
+cu_ceds6_tsql_sc %<>% gsub("Progress_([ABC])Indicator",  "Progress_\\1_Indicator", ., perl = T)
+cu_ceds6_tsql_sc %<>% gsub("Rating_([ABC])Id",  "Rating_\\1_Id", ., perl = T)
+# complicated Issues
+cu_ceds6_tsql_sc %<>% gsub("Title_I(II)*",  "Title_I\\1_", ., perl = T)
+cu_ceds6_tsql_sc %<>% gsub("Pre_*K(indergarten)*",  "Pre_K\\1_", ., perl = T, ignore.case = T)
+
+# Abbreviation Issues
+sc <- function(dat, ABBR) {
+   gsub(paste0("(_|\\[)", ABBR, "([A-Z])"),  paste0("\\1", ABBR, "_\\2"), dat, perl = T)
+}
+cu_ceds6_tsql_sc %<>% sc(., "EL")
+cu_ceds6_tsql_sc %<>% sc(., "K12")
+cu_ceds6_tsql_sc %<>% sc(., "ECEAP")
+cu_ceds6_tsql_sc %<>% sc(., "IDEA")
+cu_ceds6_tsql_sc %<>% sc(., "NCAA")
+cu_ceds6_tsql_sc %<>% sc(., "NCES")
+cu_ceds6_tsql_sc %<>% sc(., "DIF")
+cu_ceds6_tsql_sc %<>% sc(., "IEP")
+cu_ceds6_tsql_sc %<>% sc(., "COS")
+cu_ceds6_tsql_sc %<>% sc(., "XPK")
+cu_ceds6_tsql_sc %<>% sc(., "SCED")
+cu_ceds6_tsql_sc %<>% sc(., "LEA")
 
 
 ###### 3. Write Out into Snake_Case directory ######
